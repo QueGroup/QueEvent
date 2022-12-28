6a7ef9a3b591f2510d7805cea4afce55cfe854a8
vyual <i@alisoff.ru>
2022-12-28 14:32:09 +0300

    1.0 release

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from events.views import index, by_event_type
+from events.views import index, by_event_type, EventCreateView
 
 urlpatterns = [
+    path('add/', EventCreateView.as_view(), name='add'),
     path('<int:event_type_id>/', by_event_type, name='by_event_type'),
     path('', index, name='index')
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from events.forms import EventForm
 from events.models import Event, EventType
+
+
+class EventCreateView(CreateView):
+    template_name = 'events/create.html'
+    form_class = EventForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['event_types'] = EventType.objects.all()
+        return context
 
 
 # Create your views here.
